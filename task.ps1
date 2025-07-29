79b26415d928527d9f58efef35a0db968623eee5
Nazar Makivchuk <nazar.makivchuk@gmail.com>
2025-07-29 17:20:01 +0300

    change dev-branch to main

--- task.ps1
+++ task.ps1
@@ -46,7 +46,7 @@ $Params = @{
     Publisher          = 'Microsoft.Azure.Extensions'
     ExtensionType      = 'CustomScript'
     TypeHandlerVersion = '2.1'
-    Settings  = @{fileUris = @('https://raw.githubusercontent.com/nmakivchuk/azure_task_12_deploy_app_with_vm_extention/develop/install-app.sh');
+    Settings  = @{fileUris = @('https://raw.githubusercontent.com/nmakivchuk/azure_task_12_deploy_app_with_vm_extention/main/install-app.sh');
     commandToExecute = 'bash ./install-app.sh'
     }
 }
